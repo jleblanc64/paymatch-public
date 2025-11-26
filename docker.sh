9a3f6cffc8f6f47edef6373a0f159bac2eeabd43
charles.dabadie <charles.dabadie@sympheny.com>
2025-11-26 10:01:59 +0100

    fix docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,7 @@
 docker build -t app_cars .
+
+# optional: kill previously started app
 docker ps --filter "publish=8888" -q | xargs -r docker stop \
  && docker ps -a --filter "publish=8888" -q | xargs -r docker rm
-docker run -v ~/.aws:/root/.aws:ro -e AWS_PROFILE=charles-perso -p 8888:80 app_cars
+
+docker run -p 8888:80 app_cars
